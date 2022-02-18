7359b1357baaa9e6032c2f79cf9228af01e3d6e6
sistiri <sistiri@yahoo.com>
2022-02-18 15:27:58 +0100

    Understanding Type Aliases

--- basics.ts
+++ basics.ts
@@ -18,20 +18,21 @@ isInstructor = true;
 let hobbies: string[];
 hobbies = ["Sports", "Cooking"];
 
-let person: {
-  name: string;
-  age: number;
-};
+// Type Alias
+type Person = {
+    name: string;
+    age: number;
+  };
+
+
+let person: Person;
 
 person = {
   name: "Max",
   age: 32,
 };
 
-let people: {
-  name: string;
-  age: number;
-}[];
+let people: Person[];
 
 // Type interference
 
