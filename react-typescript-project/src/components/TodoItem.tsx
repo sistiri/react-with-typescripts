d4a3f5f8323bd92b9e80d34685dea64d36fe81df
sistiri <sistiri@yahoo.com>
2022-02-21 14:23:17 +0100

    Removing a Todo

--- react-typescript-project/src/components/TodoItem.tsx
+++ react-typescript-project/src/components/TodoItem.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 import classes from './TodoItem.module.css'
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{ key: string; id: string; text: string; onRemoveTodo:() => void}> = (props) => {
+
+  return <li id={props.id} className={classes.item} onClick={props.onRemoveTodo}>{props.text}</li>;
 };
 
 export default TodoItem;
