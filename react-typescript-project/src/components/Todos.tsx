ffdc6a9212d3c58fa5326c382f981f808339226e
sistiri <sistiri@yahoo.com>
2022-02-21 08:56:41 +0100

    Adding 'React.FC' Type Component => TodoItem

--- react-typescript-project/src/components/Todos.tsx
+++ react-typescript-project/src/components/Todos.tsx
@@ -1,11 +1,13 @@
+import { utimes } from "fs";
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text}/>
       ))}
     </ul>
   );
