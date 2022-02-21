4cbef38b151648ca52260dafa5c6991583d166cd
sistiri <sistiri@yahoo.com>
2022-02-21 15:07:56 +0100

    Using The Context API & TypeScript

--- react-typescript-project/src/components/NewTodo.tsx
+++ react-typescript-project/src/components/NewTodo.tsx
@@ -1,28 +1,30 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
+import { TodosContext } from "../store/todos-context";
 
-import classes from './NewTodo.module.css'
+import classes from "./NewTodo.module.css";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const enteredText = todoTextInputRef.current!.value
+    const enteredText = todoTextInputRef.current!.value;
 
-    if(enteredText.trim().length === 0) {
-        // throw error
-        return
+    if (enteredText.trim().length === 0) {
+      // throw error
+      return;
     }
 
-    props.onAddTodo(enteredText)
-
+   todosCtx.addTodo(enteredText);
   };
 
   return (
     <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" ref={todoTextInputRef}/>
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
